a398d55e17304cfb5e53560387c5c16d2213e6f2
ayaishi1213 <shitianxuannai958@gmail.com>
2021-04-30 13:42:18 +0900

    post controller作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,19 @@
 class PostsController < ApplicationController
   def index   
+    @posts = Post.order("created_at DESC")
   end
 
   def new
-    @post = Post.new()
+    @post = Post.new
   end
 
   def create
-    @post =
+    Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:category_id, :title, :article).merge(user_id: current_user.id)
   end
 end
