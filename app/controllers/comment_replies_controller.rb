9fdc5230b3d14adbc3ea6e53ad9977f9db517e87
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-04 11:52:06 +0900

    createアクション記述

--- app/controllers/comment_replies_controller.rb
+++ app/controllers/comment_replies_controller.rb
@@ -1,5 +1,22 @@
 class CommentRepliesController < ApplicationController
-  def create
-    
+
+  def new
+    @comment_reply = CommentReply.new
   end
+  
+  def create
+    @post = Post.find(params[:id])
+    @comment_reply = @post.comment_replies.build(comment_reply_params)
+    @comment_reply.comment_id = params[:comment_id]
+    @comment_replies = @post.comment_replies.include(:user).recent
+    if @comment_reply.save
+      render :index
+    end
+  end
+end
+
+private
+
+def comment_reply_params
+  params.require(:comment_reply).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id], comment_id: params[:comment_id])
 end
