338d22a52c59d7338ed61ef25fd30e125d367b29
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-04 12:08:07 +0900

    リセット

--- app/controllers/comment_replies_controller.rb
+++ app/controllers/comment_replies_controller.rb
@@ -1,22 +1,22 @@
-class CommentRepliesController < ApplicationController
+# class CommentRepliesController < ApplicationController
 
-  def new
-    @comment_reply = CommentReply.new
-  end
+#   def new
+#     @comment_reply = CommentReply.new
+#   end
   
-  def create
-    @post = Post.find(params[:id])
-    @comment_reply = @post.comment_replies.build(comment_reply_params)
-    @comment_reply.comment_id = params[:comment_id]
-    @comment_replies = @post.comment_replies.include(:user).recent
-    if @comment_reply.save
-      render :index
-    end
-  end
-end
+#   def create
+#     @post = Post.find(params[:id])
+#     @comment_reply = @post.comment_replies.build(comment_reply_params)
+#     @comment_reply.comment_id = params[:comment_id]
+#     @comment_replies = @post.comment_replies.include(:user).recent
+#     if @comment_reply.save
+#       render :index
+#     end
+#   end
+# end
 
-private
+# private
 
-def comment_reply_params
-  params.require(:comment_reply).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id], comment_id: params[:comment_id])
-end
+# def comment_reply_params
+#   params.require(:comment_reply).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id], comment_id: params[:comment_id])
+# end
